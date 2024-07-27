562d6fc9c0c30f68e2c33fd611ffe4c777b106e2
pete-restall <pete@restall.net>
2024-07-27 14:19:01 +0100

    Hacked around with some port pins to verify that the power supplies turn on and ramp correctly.  The ramps aren't bad.  The USB VBUS droop is probably out of spec when turning on the radio and audio regulator, though - VBUS drops about 400mV vs. the mandated 330mV max, but this would need looking at properly with some proper measurements and test setup.

--- src/firmware/debug/src/app/blinky-blinky.cc
+++ src/firmware/debug/src/app/blinky-blinky.cc
@@ -30,6 +30,7 @@ void blinkyBlinky(void)
 
 using namespace smeg::kernel::bare_metal::mcu::microchip::pic32;
 
+static volatile pic32cx1025sg41100::Port &portA(pic32cx1025sg41100::__linker_peripherals_ahbApb_apbb_portA);
 static volatile pic32cx1025sg41100::Port &portB(pic32cx1025sg41100::__linker_peripherals_ahbApb_apbb_portB);
 static volatile pic32cx1025sg41100::Port &portC(pic32cx1025sg41100::__linker_peripherals_ahbApb_apbb_portC);
 
@@ -45,9 +46,45 @@ void blinkyBlinky(void)
 
 	while (true)
 	{
-		portB.OUTTGL = 1 << 25;
-		for (auto i = 0; i < 2400000; i++)
+		for (auto n = 0; n < 20; n++)
+		{
+			portB.OUTTGL = 1 << 25;
+			for (auto i = 0; i < 2400000; i++)
+				__asm__ volatile ("nop");
+		}
+/*
+		// MSF_RCV_EN = PA04
+		// AMP_EN = PA06
+		portA.DIR = (1 << 4) | (1 << 6);
+		portA.OUTTGL = (1 << 4);
+		for (auto i = 0; i < 240000; i++)
 			__asm__ volatile ("nop");
+		portA.OUTTGL = (1 << 6);
+
+		// DS = PC05
+		// STCP = PC06
+		// SHCP = PC07
+		portC.DIRSET = 0b111 << 5;
+		portC.OUTCLR = 0b110 << 5;
+		portC.OUTSET = 0b001 << 5;
+		for (auto i = 0; i < 8; i++)
+		{
+			portC.OUTSET = 0b100 << 5;
+			__asm__ volatile ("nop");
+			__asm__ volatile ("nop");
+			portC.OUTCLR = 0b100 << 5;
+			__asm__ volatile ("nop");
+			__asm__ volatile ("nop");
+		}
+
+		portC.OUTSET = 0b010 << 5;
+		__asm__ volatile ("nop");
+		__asm__ volatile ("nop");
+		portC.OUTCLR = 0b010 << 5;
+
+		while (true)
+			;;
+*/
 	}
 }
 
