dd8a0cf2c60a1e5e30c403fcd2e0f85c0e3bff9c
pete-restall <pete@restall.net>
2024-07-28 15:18:49 +0100

    Added a simple example of what I would like the tasks and kernel API to look like.  This is now something that can be worked towards in a top-down manner.

---
+++ src/firmware/debug/src/apps/debug/BlinkyBlinkyTask.cc
@@ -0,0 +1,33 @@
+#include "kernel/Userspace.hh"
+#include "kernel/drivers/GpioDriver/Userspace.hh"
+#include "kernel/drivers/SoftwareDelayDriver/Userspace.hh"
+
+using namespace smeg::kernel::userspace;
+using namespace smeg::kernel::drivers::gpio_driver::userspace;
+using namespace smeg::kernel::drivers::software_delay_driver::userspace;
+
+namespace restall::msf_owl_clock::debug
+{
+	class BlinkyBlinkyTask
+	{
+	private:
+		BlockingSoftwareDelay<500ms> delay;
+		GpioSharedPin<DigitalOutput, 81> led;
+
+	public:
+		BlinkyBlinkyTask(KernelApi<GpioDriver, SoftwareDelayDriver> &kernel) :
+			delay(kernel.usingSoftwareDelayDriver().createFrom<decltype(delay)>()),
+			led(kernel.usingGpioDriver().getPinFrom<decltype(led)>())
+		{
+		}
+
+		void run(void)
+		{
+			while (true)
+			{
+				this->delay.wait();
+				this->led.toggle();
+			}
+		}
+	};
+}
