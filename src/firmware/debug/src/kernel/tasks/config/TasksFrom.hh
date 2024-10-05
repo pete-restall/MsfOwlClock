0059d340cc4511bee1a0445b8c4b2ef0114ea57f
pete-restall <pete@restall.net>
2024-10-05 20:02:09 +0100

    Added TasksFrom::Associations to allow an easy way to get at a task's ID (essentially its index).

--- src/firmware/debug/src/kernel/tasks/config/TasksFrom.hh
+++ src/firmware/debug/src/kernel/tasks/config/TasksFrom.hh
@@ -1,7 +1,10 @@
 #ifndef __SMEG_KERNEL_TASKS_CONFIG_TASKSFROM_HH
 #define __SMEG_KERNEL_TASKS_CONFIG_TASKSFROM_HH
+#include <cstdint>
 #include <tuple>
 #include "IHaveConfigForTasks.hh"
+#include "ResourceToTaskAssociation.hh"
+#include "TaskConfigsFrom.hh"
 
 namespace smeg::kernel::tasks::config
 {
@@ -9,29 +12,69 @@ namespace smeg::kernel::tasks::config
 	class TasksFrom
 	{
 	private:
-		template <typename T>
+		template <std::size_t TaskId, typename T>
 		struct AsTuple;
 
-		template <IHaveConfigForSimpleTask T>
-		struct AsTuple<T>
+		template <std::size_t TaskId, IHaveConfigForSimpleTask TTaskConfig>
+		struct AsTuple<TaskId, TTaskConfig>
 		{
-			using Types = std::tuple<typename T::Type>;
+			using Associations = std::tuple<ResourceToTaskAssociation<typename TTaskConfig::Type, TaskId>>;
 		};
 
-		template <IHaveConfigForOverlaidTasks T>
-		struct AsTuple<T>
+		template <std::size_t FirstTaskId, IHaveConfigForOverlaidTasks TTaskConfig>
+		class AsTuple<FirstTaskId, TTaskConfig>
 		{
-			using Types = T::Types;
+		private:
+			template <std::size_t TaskId, typename TTaskHead, typename... TTaskTail>
+			static consteval auto typesFromOverlayConfig(std::tuple<TTaskHead, TTaskTail...>)
+			{
+				return std::tuple_cat(
+					std::tuple<ResourceToTaskAssociation<TTaskHead, TaskId>>{},
+					typesFromOverlayConfig<TaskId + 1>(std::tuple<TTaskTail...>{}));
+			}
+
+			template <std::size_t TaskId>
+			static consteval auto typesFromOverlayConfig(std::tuple<>)
+			{
+				return std::tuple<>{};
+			}
+
+		public:
+			using Associations = decltype(typesFromOverlayConfig<FirstTaskId>(typename TTaskConfig::Types{}));
 		};
 
-		template <typename... TTaskConfigs>
-		static constexpr auto tasksFrom(std::tuple<TTaskConfigs...>)
+		template <std::size_t TaskId, typename TTaskConfigHead, typename... TTaskConfigTail>
+		static consteval auto taskAssociationsFrom(std::tuple<TTaskConfigHead, TTaskConfigTail...>)
 		{
-			return std::tuple_cat((typename AsTuple<TTaskConfigs>::Types{}) ...);
+			using TaskConfigHeadAssociations = typename AsTuple<TaskId, TTaskConfigHead>::Associations;
+			constexpr auto nextTaskId = TaskId + std::tuple_size_v<TaskConfigHeadAssociations>;
+			return std::tuple_cat(
+				TaskConfigHeadAssociations{},
+				taskAssociationsFrom<nextTaskId>(std::tuple<TTaskConfigTail...>{}));
+		}
+
+		template <std::size_t TaskId>
+		static consteval auto taskAssociationsFrom(std::tuple<>)
+		{
+			return std::tuple<>{};
+		}
+
+		template <typename TTaskAssociationHead, typename... TTaskAssociationTail>
+		static consteval auto tasksFrom(std::tuple<TTaskAssociationHead, TTaskAssociationTail...>)
+		{
+			return std::tuple_cat(
+				std::tuple<typename TTaskAssociationHead::ResourceType>{},
+				tasksFrom(std::tuple<TTaskAssociationTail...>{}));
+		}
+
+		static consteval auto tasksFrom(std::tuple<>)
+		{
+			return std::tuple<>{};
 		}
 
 	public:
-		using Types = decltype(tasksFrom(typename TConfig::Tasks{}));
+		using Associations = decltype(taskAssociationsFrom<0>(typename TConfig::Tasks{}));
+		using Types = decltype(tasksFrom(Associations{}));
 	};
 }
 
--- src/firmware/debug/tests/unit/kernel/tasks/config/TasksFromTest.cc
+++ src/firmware/debug/tests/unit/kernel/tasks/config/TasksFromTest.cc
@@ -4,6 +4,7 @@
 #include <mettle/matchers.hpp>
 
 #include "kernel/tasks/config/TasksFrom.hh"
+#include "kernel/tasks/config/ResourceToTaskAssociation.hh"
 #include "kernel/tasks/config/dsl/DefaultTaskStack.hh"
 
 #include "DummyTask.hh"
@@ -86,6 +87,167 @@ namespace smeg::tests::unit::kernel::tasks::config
 			};
 
 			expect(typeid(typename TasksFrom<Config>::Types) == typeid(std::tuple<DummyTask<1>, DummyTask<2>, DummyTask<3>>), equal_to(true));
+		});
+
+		unit.test("Types_getWhenPassedConfigWithMixOfSimpleAndOverlaidTasks_expectTupleOfFlattenedTypes", []()
+		{
+			struct Config
+			{
+				struct FirstSimpleTask
+				{
+					using Type = DummyTask<10>;
+				};
+
+				struct OverlaidTasks
+				{
+					using Types = std::tuple<DummyTask<9>, DummyTask<8>, DummyTask<7>>;
+				};
+
+				struct SecondSimpleTask
+				{
+					using Type = DummyTask<6>;
+				};
+
+				struct ThirdSimpleTask
+				{
+					using Type = DummyTask<5>;
+				};
+
+				using Tasks = std::tuple<FirstSimpleTask, OverlaidTasks, SecondSimpleTask, ThirdSimpleTask, OverlaidTasks, SecondSimpleTask>;
+			};
+
+			expect(typeid(typename TasksFrom<Config>::Types) == typeid(
+				std::tuple<
+					DummyTask<10>,
+					DummyTask<9>,
+					DummyTask<8>,
+					DummyTask<7>,
+					DummyTask<6>,
+					DummyTask<5>,
+					DummyTask<9>,
+					DummyTask<8>,
+					DummyTask<7>,
+					DummyTask<6>>),
+				equal_to(true));
+		});
+
+		unit.test("Associations_getWhenPassedConfigWithEmptyTasks_expectEmptyTuple", []()
+		{
+			struct EmptyConfig
+			{
+				using Tasks = std::tuple<>;
+			};
+
+			expect(std::tuple_size_v<typename TasksFrom<EmptyConfig>::Associations>, equal_to(0));
+		});
+
+		unit.test("Associations_getWhenPassedConfigWithSingleTask_expectTupleOfSameTypeWithIndex0", []()
+		{
+			struct Config
+			{
+				struct LonelyTask
+				{
+					using Type = DummyTask<14>;
+				};
+
+				using Tasks = std::tuple<LonelyTask>;
+			};
+
+			expect(typeid(typename TasksFrom<Config>::Associations) == typeid(std::tuple<ResourceToTaskAssociation<DummyTask<14>, 0>>), equal_to(true));
+		});
+
+		unit.test("Associations_getWhenPassedConfigWithMultipleTasks_expectTupleOfSameTypesWithIncrementingIndices", []()
+		{
+			struct Config
+			{
+				struct FirstTask
+				{
+					using Type = DummyTask<5>;
+				};
+
+				struct RepeatedTask
+				{
+					using Type = DummyTask<-12>;
+				};
+
+				struct ThirdTask
+				{
+					using Type = DummyTask<3>;
+				};
+
+				using Tasks = std::tuple<FirstTask, RepeatedTask, ThirdTask, RepeatedTask, RepeatedTask>;
+			};
+
+			expect(typeid(typename TasksFrom<Config>::Associations) == typeid(
+				std::tuple<
+					ResourceToTaskAssociation<DummyTask<5>, 0>,
+					ResourceToTaskAssociation<DummyTask<-12>, 1>,
+					ResourceToTaskAssociation<DummyTask<3>, 2>,
+					ResourceToTaskAssociation<DummyTask<-12>, 3>,
+					ResourceToTaskAssociation<DummyTask<-12>, 4>>),
+				equal_to(true));
+		});
+
+		unit.test("Associations_getWhenPassedConfigWithOverlaidTasks_expectTupleOfFlattenedTypesWithIncrementingIndices", []()
+		{
+			struct Config
+			{
+				struct OverlaidTasks
+				{
+					using Types = std::tuple<DummyTask<2>, DummyTask<5>, DummyTask<7>>;
+				};
+
+				using Tasks = std::tuple<OverlaidTasks>;
+			};
+
+			expect(typeid(typename TasksFrom<Config>::Associations) == typeid(
+				std::tuple<
+					ResourceToTaskAssociation<DummyTask<2>, 0>,
+					ResourceToTaskAssociation<DummyTask<5>, 1>,
+					ResourceToTaskAssociation<DummyTask<7>, 2>>),
+				equal_to(true));
+		});
+
+		unit.test("Associations_getWhenPassedConfigWithMixOfSimpleAndOverlaidTasks_expectTupleOfFlattenedTypesWithIncrementingIndices", []()
+		{
+			struct Config
+			{
+				struct FirstSimpleTask
+				{
+					using Type = DummyTask<101>;
+				};
+
+				struct OverlaidTasks
+				{
+					using Types = std::tuple<DummyTask<493>, DummyTask<88>, DummyTask<173789>>;
+				};
+
+				struct SecondSimpleTask
+				{
+					using Type = DummyTask<0>;
+				};
+
+				struct ThirdSimpleTask
+				{
+					using Type = DummyTask<-1>;
+				};
+
+				using Tasks = std::tuple<FirstSimpleTask, OverlaidTasks, SecondSimpleTask, ThirdSimpleTask, OverlaidTasks, SecondSimpleTask>;
+			};
+
+			expect(typeid(typename TasksFrom<Config>::Associations) == typeid(
+				std::tuple<
+					ResourceToTaskAssociation<DummyTask<101>, 0>,
+					ResourceToTaskAssociation<DummyTask<493>, 1>,
+					ResourceToTaskAssociation<DummyTask<88>, 2>,
+					ResourceToTaskAssociation<DummyTask<173789>, 3>,
+					ResourceToTaskAssociation<DummyTask<0>, 4>,
+					ResourceToTaskAssociation<DummyTask<-1>, 5>,
+					ResourceToTaskAssociation<DummyTask<493>, 6>,
+					ResourceToTaskAssociation<DummyTask<88>, 7>,
+					ResourceToTaskAssociation<DummyTask<173789>, 8>,
+					ResourceToTaskAssociation<DummyTask<0>, 9>>),
+				equal_to(true));
 		});
 	});
 }
