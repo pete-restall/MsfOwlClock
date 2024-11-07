0a97f1159408304072520f5af37b51e938ebeec6
pete-restall <pete@restall.net>
2024-11-07 17:32:28 +0000

    Added a TODO, regarding stack alignment, for later.

--- src/firmware/debug/src/kernel/tasks/config/TaskStackNumberOfSlotsFor.hh
+++ src/firmware/debug/src/kernel/tasks/config/TaskStackNumberOfSlotsFor.hh
@@ -18,6 +18,10 @@ namespace smeg::kernel::tasks::config
 //		static constexpr auto numberOfContextSwitchingSlots = Max<TupleReduction<TaskContextSwitchingConfigs, TaskContextSwitchingConfigToNumberOfSlots>>::value;
 		static constexpr auto numberOfContextSwitchingSlots = 32; // TODO: Effectively a random number, just to get the compilation to pass until the context-switching config functionality is created
 
+// Some considerations for ARM - not necessarily other processors.
+// 1. Ensure that the number of bytes is a multiple of 8 to ensure alignment (the linker script aligns the lowest memory address, but the stack starts from the highest)
+// 2. ARM 'push' is essentially '*(--sp) = val', which means we're losing one word if we set [MP]SP to the highest memory address; we need to use 'highest memory address + 1'
+
 	public:
 		static constexpr auto value = numberOfContextSwitchingSlots + TTaskStackConfig::Resource::numberOfSlots;
 	};
